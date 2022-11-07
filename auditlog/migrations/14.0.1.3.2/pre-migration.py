f5d1cb9868572ee52b33ed681b678882129bfa4a
Alexei Rivera <alexeirivera87@gmail.com>
2022-11-07 15:09:24 -0500

    [#18367] auditlog: fill of auditlog_log_line columns (sql)

--- auditlog/migrations/14.0.1.3.2/pre-migration.py
+++ auditlog/migrations/14.0.1.3.2/pre-migration.py
@@ -18,7 +18,17 @@ def migrate(cr, version):
     ADD COLUMN IF NOT EXISTS model_model VARCHAR;
     ALTER TABLE auditlog_log_line
     ADD COLUMN IF NOT EXISTS field_name VARCHAR,
-    ADD COLUMN IF NOT EXISTS field_description VARCHAR;
+    ADD COLUMN IF NOT EXISTS field_description VARCHAR,
+    ADD COLUMN IF NOT EXISTS name VARCHAR,
+    ADD COLUMN IF NOT EXISTS model_id INTEGER,
+    ADD COLUMN IF NOT EXISTS model_name VARCHAR,
+    ADD COLUMN IF NOT EXISTS model_model VARCHAR,
+    ADD COLUMN IF NOT EXISTS res_id INTEGER,
+    ADD COLUMN IF NOT EXISTS user_id INTEGER,
+    ADD COLUMN IF NOT EXISTS method VARCHAR,
+    ADD COLUMN IF NOT EXISTS http_session_id INTEGER,
+    ADD COLUMN IF NOT EXISTS http_request_id INTEGER,
+    ADD COLUMN IF NOT EXISTS log_type VARCHAR;
     ALTER TABLE auditlog_rule
     ADD COLUMN IF NOT EXISTS model_name VARCHAR,
     ADD COLUMN IF NOT EXISTS model_model VARCHAR;
@@ -47,16 +57,34 @@ def migrate(cr, version):
     WHERE im.id = al.model_id AND model_name IS NULL
     """
     )
+    # logger.info(
+    #     "Preemtive fill of auditlog_log_line columns: 'field_name' and"
+    #     " 'field_description'."
+    # )
+    # cr.execute(
+    #     """
+    # UPDATE auditlog_log_line al
+    # SET field_name = imf.name, field_description = imf.field_description
+    # FROM ir_model_fields imf
+    # WHERE imf.id = al.field_id AND field_name IS NULL
+    # """
+    # )
     logger.info(
-        "Preemtive fill of auditlog_log_line columns: 'field_name' and"
-        " 'field_description'."
+        "Preemptive fill of auditlog_log_line columns:"
+        "field_name, field_description"
+        "name, model_id, model_name, model_model, res_id, user_id, method,"
+        "http_session_id, http_request_id, log_type."
     )
     cr.execute(
         """
     UPDATE auditlog_log_line al
-    SET field_name = imf.name, field_description = imf.field_description
-    FROM ir_model_fields imf
-    WHERE imf.id = al.field_id AND field_name IS NULL
+    SET field_name = imf.name, field_description = imf.field_description,
+    name = log.name, model_id = log.model_id, model_name = log.model_name,
+    model_model = log.model_model, res_id = log.res_id, user_id = log.user_id,
+    method = log.method, http_session_id = log.http_session_id, http_request_id = log.http_request_id,
+    log_type = log.log_type
+    FROM ir_model_fields imf, auditlog_log log
+    WHERE imf.id = al.field_id AND field_name IS NULL AND log.id = al.log_id
     """
     )
     logger.info(
